329c3ef8a10f1de0a1217580c9617ce661a51734
Aman <meamanx7@gmail.com>
2025-10-09 01:45:32 +0530

    Firt Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3006
+
+app.post('/ans1', (req, res) => {
+
+    res.send("you hit server1");
+})
+
+app.post('/ans2', (req, res) => {
+    res.send("you hit sever2");
+    
+})
+
+app.post('/', (req, res) => {
+    console.log(req.headers);
+    res.send("you hit main server");
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
